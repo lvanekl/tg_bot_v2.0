27ac321d2942d30e3c41e5c7190a86d284763934
vanek <23.gorjachev.i@gmail.com>
2023-07-11 12:50:24 +0300

    Добавил отлавливание ошибки в удаленных чатах

--- bot/bot_logics/basic/polls.py
+++ bot/bot_logics/basic/polls.py
@@ -1,6 +1,9 @@
 import logging
-from aiogram import types
 
+from aiogram import types
+from aiogram.utils.exceptions import Unauthorized
+
+from chats.models import Chat
 from env.env import LOG_PATH, LOGGING_LEVEL
 
 logging.basicConfig(level=LOGGING_LEVEL, filename=LOG_PATH, filemode="w",
@@ -13,4 +16,10 @@ async def send_poll(chat_id: int, poll: dict):
     print(f'Отправка голосования в чат {chat_id}')
     print(poll)
     print()
-    await bot.send_poll(chat_id=chat_id, **poll, is_anonymous=False)
+    try:
+        await bot.send_poll(chat_id=chat_id, **poll, is_anonymous=False)
+    except Unauthorized:
+        Chat.objects.get(chat_id=chat_id).delete()
+        print(chat_id, 'был удален в телеграмме, больше нет к нему доступа, стираю его из базы')
+    except Exception as e:
+        logging.error(e)
